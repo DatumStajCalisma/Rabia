99386403f76c9375771aac853de6f985cf57a4bc
RabiaKuran <59939284+RabiaKuran@users.noreply.github.com>
2022-07-21 14:58:45 +0300

    Java Çalışma 6 PostgreSQL

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,13 +2,10 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
-import kodlamaio.northwind.core.utilities.results.DataResult;
-import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
-	DataResult<List<Product>>  getAll();
-	Result add(Product product);
+	List<Product> getAll();
 	
 
 }
--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer>  { //interface intercase i extend eder.
-	
-	
 
 }
